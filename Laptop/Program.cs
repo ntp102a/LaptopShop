1ef353af9bff5816a8e80c488dbb903f93c5aa93
ntp102a <ntp1022k2@gmail.com>
2024-12-09 14:50:39 +0700

    update Recommend Product

--- Laptop/Program.cs
+++ Laptop/Program.cs
@@ -53,9 +53,9 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-            name: "areas",
-            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-          );
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
